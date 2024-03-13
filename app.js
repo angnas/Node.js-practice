b1a55b23e1c997088d27a89511d0f2ccfd21428c
Angela Naseri <f.naseri1993@gmail.com>
2024-03-13 11:43:59 +0100

    more practice

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-const EventEmitter = require('node:events');
-const emitter = new EventEmitter();
+const http = require('node:http');
 
-// Register a listener
-emitter.addListener('messageLogged', (arg) => {
-    console.log('Listener called', arg);
-})
 
-//Raise an event
-emitter.emit('messageLogged' ,{ id : 1, url: 'http://'});
+const server = http.createServer((req, res) =>{
+    if (req.url === '/' ) {
+        res.write('Hello World');
+        res.end();
+    }
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]))
+        res.end();
+    }
+});
+
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
+
+
+
+/////////////////////////////////
+// const EventEmitter = require('node:events');
+// const emitter = new EventEmitter();
+
+// // Register a listener
+// emitter.addListener('messageLogged', (arg) => {
+//     console.log('Listener called', arg);
+// })
+
+// //Raise an event
+// emitter.emit('messageLogged' ,{ id : 1, url: 'http://'});
+
+//Raise: logging (data: message)
+
 
 
 //////////////////////////////////
